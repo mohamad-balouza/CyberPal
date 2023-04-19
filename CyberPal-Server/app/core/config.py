1e1ae475d5706e37a6e4c71703443c7cfc89eecf
unknown <mohammad.balouza@gmail.com>
2023-04-19 21:43:53 +0300

    Added the database to the server

--- CyberPal-Server/app/core/config.py
+++ CyberPal-Server/app/core/config.py
@@ -5,5 +5,11 @@ class Settings(BaseSettings):
     APP_NAME: str
     TEMP_EMAIL: str
 
+    DB_TYPE: str
+    DB_USERNAME: str
+    DB_PASSWORD: str
+    DB_NAME: str
+    DB_SERVER: str
+
     class Config:
         env_file = ".env"
---
+++ CyberPal-Server/app/database/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from app.core.config import Settings
+
+
+SQLALCHEMY_DATABASE_URL = f"{Settings.DB_TYPE}://{Settings.DB_USERNAME}:{Settings.DB_PASSWORD}@{Settings.DB_SERVER}/{Settings.DB_NAME}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
