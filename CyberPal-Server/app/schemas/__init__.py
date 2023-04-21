dd457bd7e16d9437ee85fac8758aae6666fd067a
unknown <mohammad.balouza@gmail.com>
2023-04-21 03:20:40 +0300

    Added imports that will be used in the schemas

--- CyberPal-Server/app/schemas/__init__.py
+++ CyberPal-Server/app/schemas/__init__.py
@@ -1,10 +1,10 @@
 from .user import User, UserCreate, UserUpdate, UserInDB
-from .user_type import UserType
-from .openvpn_file import OpenvpnFile
-from .script import Script
-from .favorite_script import FavoriteScript
-from .tool import Tool
-from .flag import Flag
-from .used_flag import UsedFlag
-from .schedule import Schedule
-from .scheduled_flag import ScheduledFlag
+from .user_type import UserType, UserTypeCreate, UserTypeUpdate, UserTypeInDB
+from .openvpn_file import OpenvpnFile, OpenvpnFileCreate, OpenvpnFileUpdate, OpenvpnFileInDB
+from .script import Script, ScriptCreate, ScriptUpdate, ScriptInDB
+from .favorite_script import FavoriteScript, FavoriteScriptCreate, FavoriteScriptUpdate, FavoriteScriptInDB
+from .tool import Tool, ToolCreate, ToolUpdate, ToolInDB
+from .flag import Flag, FlagCreate, FlagUpdate, FlagInDB
+from .used_flag import UsedFlag, UsedFlagCreate, UsedFlagUpdate, UsedFlagInDB
+from .schedule import Schedule, ScheduleCreate, ScheduleUpdate, ScheduleInDB
+from .scheduled_flag import ScheduledFlag, ScheduledFlagCreate, ScheduledFlagUpdate, ScheduledFlagInDB
