eded5ba755ebea5bf152857a89c4adc9baa2f527
unknown <mohammad.balouza@gmail.com>
2023-04-21 17:58:58 +0300

    Added the get method in the base crud

---
+++ CyberPal-Server/app/crud/base.py
@@ -0,0 +1,15 @@
+from typing import TypeVar, Generic, Type, Any, Optional
+from pydantic import BaseModel
+from app.database.database import Base
+from sqlalchemy.orm import Session
+
+ModelType = TypeVar("ModelType", bound=Base)
+CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
+UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
+
+class CrudBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
+    def __init__(self, model: Type[ModelType]):
+        self.model = model
+
+    def get(self, db: Session, id: Any) -> Optional[ModelType]:
+        return db.query(self.model).filter(self.model.id == id).first()
