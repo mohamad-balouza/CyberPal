8e23d3a7e35a8cc9e22723e4cfde6419b546a866
unknown <mohammad.balouza@gmail.com>
2023-04-23 04:21:07 +0300

    Added the routers basic structure

--- CyberPal-Server/app/api/api_v1/api_routers.py
+++ CyberPal-Server/app/api/api_v1/api_routers.py
@@ -1,3 +1,11 @@
 from fastapi import APIRouter
+from app.api.api_v1.routers import users, tools, scripts, schedules, openvpn_files, login
 
 api_router = APIRouter()
+
+api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(tools.router, prefix="/tools", tags=["tools"])
+api_router.include_router(scripts.router, prefix="/scripts", tags=["scripts"])
+api_router.include_router(schedules.router, prefix="/schedules", tags=["schedules"])
+api_router.include_router(openvpn_files.router, prefix="/openvpn_files", tags=["openvpn_files"])
+api_router.include_router(login.router, prefix="/login", tags=["login"])
--- CyberPal-Server/app/main.py
+++ CyberPal-Server/app/main.py
@@ -12,10 +12,6 @@ app = FastAPI()
 # Creating the tables in the database
 Base.metadata.create_all(bind=engine)
 
-# @lru_cache()
-# def get_settings():
-#     return config.Settings()
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost", "http://localhost:3000", "http://localhost:8080" "http://localhost:8000"],
@@ -26,18 +22,18 @@ app.add_middleware(
 
 app.include_router(api_router, prefix=config.settings.API_V1_STR)
 
-from app.api.deps import getDb
-from sqlalchemy.orm import Session
-from app.crud import crud_user_type, crud_user
-from app.schemas import user, user_type
-from typing import List
+# from app.api.deps import getDb
+# from sqlalchemy.orm import Session
+# from app.crud import crud_user_type, crud_user
+# from app.schemas import user, user_type
+# from typing import List
 
-@app.get("/user_type", response_model=List[user_type.UserType])
-async def getUserTypes(db: Session = Depends(getDb), offset: int = 0, limit: int = 100):
-    user_types = crud_user_type.user_type.getMultiple(db, offset=offset, limit=limit)
-    return user_types
+# @app.get("/user_type", response_model=List[user_type.UserType])
+# async def getUserTypes(db: Session = Depends(getDb), offset: int = 0, limit: int = 100):
+#     user_types = crud_user_type.user_type.getMultiple(db, offset=offset, limit=limit)
+#     return user_types
 
-@app.get("/users", response_model=List[user.User])
-async def getUsers(db: Session = Depends(getDb), offset: int = 0, limit: int = 100):
-    users = crud_user.user.getMultiple(db, offset=offset, limit=limit)
-    return users
+# @app.get("/users", response_model=List[user.User])
+# async def getUsers(db: Session = Depends(getDb), offset: int = 0, limit: int = 100):
+#     users = crud_user.user.getMultiple(db, offset=offset, limit=limit)
+#     return users
