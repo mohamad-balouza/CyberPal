226dda5716f5c093eb2b8c4070ba9d796a5d45b1
unknown <mohammad.balouza@gmail.com>
2023-04-23 23:02:57 +0300

    Added the create with author for the crud schedule

--- CyberPal-Server/app/crud/crud_schedule.py
+++ CyberPal-Server/app/crud/crud_schedule.py
@@ -1,9 +1,19 @@
 from app.crud.base import CrudBase
 from app.models.schedule import Schedule
 from app.schemas.schedule import ScheduleCreate, ScheduleUpdate
+from sqlalchemy.orm import Session
+from fastapi.encoders import jsonable_encoder
+from typing import List
 
 class CrudSchedule(CrudBase[Schedule, ScheduleCreate, ScheduleUpdate]):
-    pass
+    def createWithAuthor(self, db: Session, *, obj_in: ScheduleCreate, user_id: int) -> Schedule:
+        obj_in_data = jsonable_encoder(obj_in)
+        db_obj = self.model(**obj_in_data, user_id=user_id)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
 
 
 schedule = CrudSchedule(Schedule)
