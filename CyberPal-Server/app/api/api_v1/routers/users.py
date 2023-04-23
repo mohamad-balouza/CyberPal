a9ba3ff1e6888503652edbbfede10211b5221f7a
unknown <mohammad.balouza@gmail.com>
2023-04-23 05:20:42 +0300

    Added the create user route

--- CyberPal-Server/app/api/api_v1/routers/users.py
+++ CyberPal-Server/app/api/api_v1/routers/users.py
@@ -11,3 +11,13 @@ def getUsers(db: Session = Depends(deps.getDb), skip: int = 0, limit: int = 100)
 
     users = crud.user.getMultiple(db, skip=skip, limit=limit)
     return users
+
+@router.post("/", response_model=schemas.User)
+def create_user(db: Session = Depends(deps.getDb), *, user_in: schemas.UserCreate) -> Any:
+
+    user = crud.user.getByEmail(db, email=user_in.email)
+    if user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="The user with this email already exists in the system.")
+    
+    user = crud.user.create(db, obj_in=user_in)
+    return user
