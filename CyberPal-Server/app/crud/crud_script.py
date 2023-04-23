5dfecaba7f319ef82842ede5bbd368ed48f6f24f
unknown <mohammad.balouza@gmail.com>
2023-04-23 22:29:41 +0300

    Added the get multiple by author for the crud script

--- CyberPal-Server/app/crud/crud_script.py
+++ CyberPal-Server/app/crud/crud_script.py
@@ -3,6 +3,7 @@ from app.models.script import Script
 from app.schemas.script import ScriptCreate, ScriptUpdate
 from sqlalchemy.orm import Session
 from fastapi.encoders import jsonable_encoder
+from typing import List
 
 class CrudScript(CrudBase[Script, ScriptCreate, ScriptUpdate]):
     def createWithAuthor(self, db: Session, *, obj_in: ScriptCreate, author_id: int) -> Script:
@@ -12,6 +13,9 @@ class CrudScript(CrudBase[Script, ScriptCreate, ScriptUpdate]):
         db.commit()
         db.refresh(db_obj)
         return db_obj
+    
+    def getMultipleByAuthor(self, db: Session, *, author_id: int, skip: int = 0, limit: int = 100) -> List[Script]:
+        return db.query(self.model).filter(Script.author_id == author_id).offset(skip).limit(limit).all()
 
 
 script = CrudScript(Script)
