9cf93b1b933cb3273ee3d5ddca9f715b4361ec13
unknown <mohammad.balouza@gmail.com>
2023-04-23 04:52:19 +0300

    Added the create user function

--- CyberPal-Server/app/crud/crud_user.py
+++ CyberPal-Server/app/crud/crud_user.py
@@ -1,6 +1,7 @@
 from app.crud.base import CrudBase
 from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate
+from app.core.security import getPasswordHash
 from sqlalchemy.orm import joinedload, Session
 from typing import List
 
@@ -13,6 +14,17 @@ class CrudUser(CrudBase[User, UserCreate, UserUpdate]):
         if user.user_type == 1:
             return True
         return False
+    
+    def create(self, db: Session, *, obj_in: UserCreate) -> User:
+        db_obj = User(
+            email=obj_in.email,
+            hashed_password=getPasswordHash(obj_in.password),
+            username=obj_in.username,
+        )
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
 
 
 user = CrudUser(User)
