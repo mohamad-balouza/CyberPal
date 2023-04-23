7e387c516cb423c42db2ffd9873ceb368f2ff102
unknown <mohammad.balouza@gmail.com>
2023-04-23 05:19:50 +0300

    Added the get users route

--- CyberPal-Server/app/api/api_v1/routers/users.py
+++ CyberPal-Server/app/api/api_v1/routers/users.py
@@ -1,3 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from typing import Any, List
+from sqlalchemy.orm import Session
+from app import schemas, crud
+from app.api import deps 
 
 router = APIRouter()
+
+@router.get("/", response_model=List[schemas.User])
+def getUsers(db: Session = Depends(deps.getDb), skip: int = 0, limit: int = 100) -> Any:
+
+    users = crud.user.getMultiple(db, skip=skip, limit=limit)
+    return users
