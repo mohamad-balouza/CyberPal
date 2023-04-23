9126e6315c1086a5168ccf3c60b08326a12934c5
unknown <mohammad.balouza@gmail.com>
2023-04-23 22:21:01 +0300

    Added the get all scripts route

--- CyberPal-Server/app/api/api_v1/routers/scripts.py
+++ CyberPal-Server/app/api/api_v1/routers/scripts.py
@@ -1,3 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from typing import Any, List
+from sqlalchemy.orm import Session
+from app import schemas, crud, models
+from app.api import deps 
 
 router = APIRouter()
+
+@router.get("/", response_model=List[schemas.Script])
+def getAllScripts(
+    db: Session = Depends(deps.getDb), 
+    skip: int = 0, 
+    limit: int = 100,
+    current_user: models.User = Depends(deps.getCurrentAdmin)
+    ) -> Any:
+    scripts = crud.script.getMultiple(db, skip=skip, limit=limit)
+    return scripts
