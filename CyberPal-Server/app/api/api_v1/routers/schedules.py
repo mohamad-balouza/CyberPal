973dc7aca92a89c1b7c3c1f41ac2f24db55f9c88
unknown <mohammad.balouza@gmail.com>
2023-04-23 23:05:42 +0300

    Added the get all schedules route

--- CyberPal-Server/app/api/api_v1/routers/schedules.py
+++ CyberPal-Server/app/api/api_v1/routers/schedules.py
@@ -1,3 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from typing import Any, List
+from sqlalchemy.orm import Session
+from app import schemas, crud, models
+from app.api import deps 
 
 router = APIRouter()
+
+@router.get("/", response_model=List[schemas.Schedule])
+def getAllSchedules(
+    db: Session = Depends(deps.getDb), 
+    skip: int = 0, 
+    limit: int = 100,
+    current_user: models.User = Depends(deps.getCurrentActiveUser)
+    ) -> Any:
+
+    if crud.user.isAdmin(current_user):
+        schedules = crud.schedule.getMultiple(db, skip=skip, limit=limit)
+    else:
+        schedules = crud.schedule.getMultipleByAuthor(db, user_id=current_user.id, skip=skip, limit=limit)
+
+    return schedules
