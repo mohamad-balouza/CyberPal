4d3b7ed87e6ab05d4248b934460afa91fc5b4c7a
unknown <mohammad.balouza@gmail.com>
2023-04-24 06:30:52 +0300

    Added the create with tool id in the crud flag

--- CyberPal-Server/app/crud/crud_flag.py
+++ CyberPal-Server/app/crud/crud_flag.py
@@ -1,9 +1,19 @@
 from app.crud.base import CrudBase
 from app.models.flag import Flag
 from app.schemas.flag import FlagCreate, FlagUpdate
+from sqlalchemy.orm import Session
+from fastapi.encoders import jsonable_encoder
+from typing import List
 
 class CrudFlag(CrudBase[Flag, FlagCreate, FlagUpdate]):
-    pass
+    def createWithToolId(self, db: Session, *, obj_in: FlagCreate, tool_id: int) -> Flag:
+        obj_in_data = jsonable_encoder(obj_in)
+        db_obj = self.model(**obj_in_data, tool_id=tool_id)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
 
 
 flag = CrudFlag(Flag)
