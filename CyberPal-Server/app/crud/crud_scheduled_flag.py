1dedc04489f68e3746fde4f654baf45263658afe
unknown <mohammad.balouza@gmail.com>
2023-04-24 06:54:04 +0300

    Added the get mutltiple by schedule id in the crud scheduled flag

--- CyberPal-Server/app/crud/crud_scheduled_flag.py
+++ CyberPal-Server/app/crud/crud_scheduled_flag.py
@@ -1,9 +1,13 @@
 from app.crud.base import CrudBase
 from app.models.scheduled_flag import ScheduledFlag
 from app.schemas.scheduled_flag import ScheduledFlagCreate, ScheduledFlagUpdate
+from sqlalchemy.orm import Session
+from fastapi.encoders import jsonable_encoder
+from typing import List
 
 class CrudScheduledFlag(CrudBase[ScheduledFlag, ScheduledFlagCreate, ScheduledFlagUpdate]):
-    pass
+    def getMultipleByAuthor(self, db: Session, *, schedule_id: int, skip: int = 0, limit: int = 100) -> List[ScheduledFlag]:
+        return db.query(self.model).filter(ScheduledFlag.schedule_id == schedule_id).offset(skip).limit(limit).all()
 
 
 scheduled_flag = CrudScheduledFlag(ScheduledFlag)
