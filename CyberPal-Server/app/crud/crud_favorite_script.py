9f42d03b996dcab3d0d4ad7b10c636e7337adc9c
unknown <mohammad.balouza@gmail.com>
2023-04-24 04:38:13 +0300

    Added the create with author for the crud favorite script

--- CyberPal-Server/app/crud/crud_favorite_script.py
+++ CyberPal-Server/app/crud/crud_favorite_script.py
@@ -1,9 +1,17 @@
 from app.crud.base import CrudBase
 from app.models.favorite_script import FavoriteScript
 from app.schemas.favorite_script import FavoriteScriptCreate, FavoriteScriptUpdate
+from sqlalchemy.orm import Session
+from fastapi.encoders import jsonable_encoder
+from typing import List
 
 class CrudFavoriteScript(CrudBase[FavoriteScript, FavoriteScriptCreate, FavoriteScriptUpdate]):
-    pass
-
+    def createWithAuthor(self, db: Session, *, obj_in: FavoriteScriptCreate, user_who_favorited_id: int) -> FavoriteScript:
+        obj_in_data = jsonable_encoder(obj_in)
+        db_obj = self.model(**obj_in_data, user_who_favorited_id=user_who_favorited_id)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
 
 favorite_script = CrudFavoriteScript(FavoriteScript)
