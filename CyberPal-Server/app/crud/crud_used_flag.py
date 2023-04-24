88ccda3e5ab5f30831fd9756f71d1aedefc16afd
unknown <mohammad.balouza@gmail.com>
2023-04-24 04:41:23 +0300

    Added the create with author function for the crud used flag

--- CyberPal-Server/app/crud/crud_used_flag.py
+++ CyberPal-Server/app/crud/crud_used_flag.py
@@ -1,9 +1,19 @@
 from app.crud.base import CrudBase
 from app.models.used_flag import UsedFlag
 from app.schemas.used_flag import UsedFlagCreate, UsedFlagUpdate
+from sqlalchemy.orm import Session
+from fastapi.encoders import jsonable_encoder
+from typing import List
 
 class CrudUsedFlag(CrudBase[UsedFlag, UsedFlagCreate, UsedFlagUpdate]):
-    pass
+    def createWithAuthor(self, db: Session, *, obj_in: UsedFlagCreate, user_id: int) -> UsedFlag:
+        obj_in_data = jsonable_encoder(obj_in)
+        db_obj = self.model(**obj_in_data, user_id=user_id)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
 
 
 used_flag = CrudUsedFlag(UsedFlag)
