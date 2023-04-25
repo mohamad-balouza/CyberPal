97535812065031479e9823343db5a4541318000d
unknown <mohammad.balouza@gmail.com>
2023-04-25 23:57:33 +0300

    Frontend: Fixed the spacing issue

--- CyberPal-Desktop/src/Pages/LoginPage/index.tsx
+++ CyberPal-Desktop/src/Pages/LoginPage/index.tsx
@@ -3,10 +3,11 @@ import Logo from '../../../assets/Logo.svg';
 import { Image } from 'primereact/image';
 import Navbar from 'Components/Navbar';
 import LoginBlock from 'Components/Loginblock';
+import './index.css'
         
 function LoginPage() {
   return (
-    <div>
+    <div className='login-page'>
         <Navbar />
         <LoginBlock />
     </div>
