f59b9357090d7a4be1d83797832a0b8172cafe6c
unknown <mohammad.balouza@gmail.com>
2023-04-25 07:42:33 +0300

    Frontend: Added the first navbar prototype

--- CyberPal-Desktop/src/Components/Navbar/index.tsx
+++ CyberPal-Desktop/src/Components/Navbar/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Logo from '../../../assets/Logo.svg';
 import { Image } from 'primereact/image';
+import "./index.css";
 
 
 function Navbar() {
   return (
-    <div>
-        <Image src={Logo} width="100" />
+    <div className='navbar'>
+        <Image src={Logo} width="60" />
+        <div className='navigation'>
+            <a>Home</a>
+            <a>About</a>
+            <a>Tools</a>
+            <a>Advanced</a>
+            <a>Contact</a>
+        </div>
+        <div className='profile-zone'>
+            <a>Signup</a>
+            <a id='chosen-one'>Signin</a>
+        </div>
     </div>
   )
 }
