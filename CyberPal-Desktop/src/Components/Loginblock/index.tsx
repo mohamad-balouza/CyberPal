86324a0d6e226fd9aada0a173ad9a762e0060fe8
unknown <mohammad.balouza@gmail.com>
2023-04-25 23:49:05 +0300

    Frontend: Added the photo for the login block

---
+++ CyberPal-Desktop/src/Components/Loginblock/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LoginImage from '../../../assets/LoginImage.svg';
+import { Image } from 'primereact/image';
+import "./index.css";
+        
+function LoginBlock() {
+  return (
+    <div className='login-block'>
+        <Image src={LoginImage} width="436.13" height='434.73'/>
+    </div>
+)
+}
+
+export default LoginBlock
--- CyberPal-Desktop/src/Pages/LoginPage/index.tsx
+++ CyberPal-Desktop/src/Pages/LoginPage/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Logo from '../../../assets/Logo.svg';
 import { Image } from 'primereact/image';
 import Navbar from 'Components/Navbar';
+import LoginBlock from 'Components/Loginblock';
         
 function LoginPage() {
   return (
     <div>
         <Navbar />
+        <LoginBlock />
     </div>
   )
 }
