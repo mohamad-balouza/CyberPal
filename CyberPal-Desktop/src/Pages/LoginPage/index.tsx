c31568150415f4c607030e4ce56562e6a0cd0d3e
unknown <mohammad.balouza@gmail.com>
2023-04-25 07:08:58 +0300

    Frontend: Tested some features

---
+++ CyberPal-Desktop/src/Pages/LoginPage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from '../../../assets/Logo.svg';
+import { Image } from 'primereact/image';
+        
+function LoginPage() {
+  return (
+    <div>
+        <Image src={Logo} width="100" />
+    </div>
+  )
+}
+
+export default LoginPage
