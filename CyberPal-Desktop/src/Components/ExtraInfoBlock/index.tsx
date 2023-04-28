26a83cc8a4b58aa3989040a9264978f154401444
unknown <mohammad.balouza@gmail.com>
2023-04-28 23:31:08 +0300

    Frontend: Added the other colors for the extra info cards

--- CyberPal-Desktop/src/Components/ExtraInfoBlock/index.tsx
+++ CyberPal-Desktop/src/Components/ExtraInfoBlock/index.tsx
@@ -8,8 +8,8 @@ function ExtraInfoBlock() {
         <h3>What We Do</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate nulla, nobis voluptate optio sapiente aspernatur impedit consequuntur itaque tempore harum.</p>
         <div className='extra-info-cards-block'>
-            <ExtraInfoCard title="Information Title" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit." button_label="click here" color="cyan" />
-            <ExtraInfoCard title="Information Title" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit." button_label="click here" color="cyan" />
+            <ExtraInfoCard title="Information Title" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit." button_label="click here" color="blue" />
+            <ExtraInfoCard title="Information Title" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit." button_label="click here" color="pink" />
             <ExtraInfoCard title="Information Title" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit." button_label="click here" color="cyan" />
         </div>
     </div>  
