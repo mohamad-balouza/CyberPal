0a35f22fe86ca00c141fde5380bf70399911f24e
unknown <mohammad.balouza@gmail.com>
2023-04-29 18:56:42 +0300

    Frontend: Added the username inputs without the password

--- CyberPal-Desktop/src/Pages/UserFormPage/index.tsx
+++ CyberPal-Desktop/src/Pages/UserFormPage/index.tsx
@@ -2,12 +2,16 @@ import AdminNavbar from 'Components/AdminNavbar';
 import AdminSidebar from 'Components/AdminSidebar';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
-import { Column } from 'primereact/column';
+import { Password } from 'primereact/password';
 import React, { useState } from 'react';
 import './index.css';
 
 function UserFormPage() {
   const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [usertype, setUsertype] = useState("");
+  const [isactive, setIsActive] = useState("");
 
   return (
     <div className='admin-page-block'>
@@ -15,11 +19,25 @@ function UserFormPage() {
       <div className='admin-page-content-block'>
         <AdminNavbar />
         <h3>User Form</h3>
-        <div className="card">
-            <span className="p-float-label">
-                <InputText id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
-                <label htmlFor="username">Username</label>
-            </span>
+        <div className='form-block'>
+            <div className="form-input-block">
+                <div className="p-float-label" style={{width: "100%", display: "flex"}}>
+                    <InputText id="username" value={username} onChange={(e) => setUsername(e.target.value)}  style={{flex: "1"}}/>
+                    <label htmlFor="username">Username</label>
+                </div>
+                <div className="p-float-label" style={{width: "49.2%", display: "flex"}}>
+                    <InputText id="email" value={email} onChange={(e) => setEmail(e.target.value)}  style={{flex: "1"}}/>
+                    <label htmlFor="email">Email</label>
+                </div>
+                <div className="p-float-label" style={{flex: "1", display: "flex"}}>
+                    <InputText id="usertype" value={usertype} onChange={(e) => setUsertype(e.target.value)}  style={{flex: "1"}}/>
+                    <label htmlFor="usertype">User Type</label>
+                </div>
+                <div className="p-float-label" style={{flex: "1",display: "flex"}}>
+                    <InputText id="isactive" value={isactive} onChange={(e) => setIsActive(e.target.value)}  style={{flex: "1"}}/>
+                    <label htmlFor="isactive">Is Active</label>
+                </div>
+            </div>
         </div>
       </div>
     </div>
