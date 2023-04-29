9749fe61096e8a3dd6ad38c6bc93701abab790f4
unknown <mohammad.balouza@gmail.com>
2023-04-29 20:39:20 +0300

    Frontend: Added the profile info block as a seperate component

---
+++ CyberPal-Desktop/src/Components/ProfileInfoBlock/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Avatar } from 'primereact/avatar';
+
+
+function ProfileInfoBlock() {
+  return (
+    <div className='profile-info-block'>
+        <div className='profile-picture-block'>
+            <Avatar label="P" size="xlarge" shape="circle" />
+        </div>
+        <div className='profile-details-block'>
+            <h3>Username</h3>
+            <p>Account Type</p>
+            <a>Edit</a>
+        </div>
+    </div>
+  )
+}
+
+export default ProfileInfoBlock
--- CyberPal-Desktop/src/Pages/ProfilePage/index.tsx
+++ CyberPal-Desktop/src/Pages/ProfilePage/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from 'Components/Navbar';
-import { Avatar } from 'primereact/avatar';
 import './index.css';
+import ProfileInfoBlock from 'Components/ProfileInfoBlock';
 
 
 function ProfilePage() {
@@ -9,16 +9,7 @@ function ProfilePage() {
     <div>
         <Navbar />
         <div className='profile-block'>
-            <div className='profile-info-block'>
-                <div className='profile-picture-block'>
-                    <Avatar label="P" size="xlarge" shape="circle" />
-                </div>
-                <div className='profile-details-block'>
-                    <h3>Username</h3>
-                    <p>Account Type</p>
-                    <a>Edit</a>
-                </div>
-            </div>
+            <ProfileInfoBlock />
             <div className='profile-contents-block'></div>
         </div>
     </div>
