4d3083eeb85feab13b9bae2a4523a8aaadb6c672
unknown <mohammad.balouza@gmail.com>
2023-04-29 21:32:14 +0300

    Frontend: Added the scripts table

--- CyberPal-Desktop/src/Components/ProfileContentsBlock/index.tsx
+++ CyberPal-Desktop/src/Components/ProfileContentsBlock/index.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import './index.css';
 import { TabView, TabPanel } from 'primereact/tabview';
 import SchedulesTable from 'Components/SchedulesTable';
+import ScriptsTable from 'Components/ScriptsTable';
         
 
 function ProfileContentsBlock() {
   return (
     <TabView>
         <TabPanel header="Schedules">
-            <SchedulesTable />
+          <SchedulesTable />
         </TabPanel>
         <TabPanel header="Scripts">
-
+          <ScriptsTable />
         </TabPanel>
         <TabPanel header="Favorite Scripts">
 
---
+++ CyberPal-Desktop/src/Components/ScriptsTable/index.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+
+function ScriptsTable() {
+    const[userscripts, setUserscripts] = useState([]);
+
+    return (
+        <div className="card">
+            <div className="card">
+                <DataTable size='small' value={userscripts} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+                        currentPageReportTemplate="{first} to {last} of {totalRecords}" emptyMessage="No Scripts Found!">
+                    <Column field="id" header="ID" style={{ width: '10%' }}></Column>
+                    <Column field="script_title" header="Title" style={{ width: '30%' }}></Column>
+                    <Column field="script_content" header="Content" style={{ width: '60%' }}></Column>
+                </DataTable>
+            </div>
+        </div>
+    )
+}
+
+export default ScriptsTable
