130d10c8142dc98a1911e71d2a50873e1f1680f8
unknown <mohammad.balouza@gmail.com>
2023-04-30 16:41:54 +0300

    tried the first test

--- CyberPal-Desktop/src/Components/ToolsMenu/index.tsx
+++ CyberPal-Desktop/src/Components/ToolsMenu/index.tsx
@@ -21,7 +21,17 @@ function ToolsMenu() {
             onTap={() => setIsOpen(!isOpen)}
         >
           <motion.div layout className="child">{isOpen ? "hello" : "How are you"}</motion.div>
-          
+
+        </motion.div>
+        <motion.div
+            layout
+            data-isOpen={isOpen}
+            initial={{ borderRadius: 50 }}
+            className="tool-menu-item"
+            onTap={() => setIsOpen(!isOpen)}
+        >
+          <motion.div layout className="child">{isOpen ? "hello" : "How are you"}</motion.div>
+
         </motion.div>
         <div className='tool-menu-item'>Hello</div>
         <div className='tool-menu-item'>Hello</div>
