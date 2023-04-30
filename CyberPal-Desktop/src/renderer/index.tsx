62f1c3c52ab95ab117bf50bb686f9973d476182f
unknown <mohammad.balouza@gmail.com>
2023-05-01 00:13:15 +0300

    Frontend: Wrapped the app in the store

--- CyberPal-Desktop/src/renderer/index.tsx
+++ CyberPal-Desktop/src/renderer/index.tsx
@@ -1,9 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { store } from '../Redux/store';
+import { Provider } from 'react-redux';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 // calling IPC exposed from preload script
 window.electron.ipcRenderer.once('ipc-example', (arg) => {
