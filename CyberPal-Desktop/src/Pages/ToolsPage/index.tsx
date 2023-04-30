171583d88f6c45df9b2ddb64e669db7a28c7a095
unknown <mohammad.balouza@gmail.com>
2023-04-30 14:29:45 +0300

    Frontend: Added the general structure of the tools page

--- CyberPal-Desktop/src/Pages/ToolsPage/index.tsx
+++ CyberPal-Desktop/src/Pages/ToolsPage/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import Navbar from 'Components/Navbar';
+import ToolsMenu from 'Components/ToolsMenu';
+import { Image } from 'primereact/image';
+import ToolsDeskImage from '../../../assets/ToolsDeskImage.png';
+import './index.css'
 
 function ToolsPage() {
   return (
-    <div>ToolsPage</div>
+    <div className='tools-page-block'>
+        <Navbar />
+        <div  className='tools-page-content-block'>
+            <ToolsMenu />
+            <Image src={ToolsDeskImage} width="550"/>
+        </div>
+    </div>
   )
 }
 
