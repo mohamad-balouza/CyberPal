bc15f5143c2b69011bf6cf22e5abee4c216bcd8b
unknown <mohammad.balouza@gmail.com>
2023-04-30 14:59:05 +0300

    Frontend: Added the first animation test

--- CyberPal-Desktop/src/Components/ToolsMenu/index.tsx
+++ CyberPal-Desktop/src/Components/ToolsMenu/index.tsx
@@ -5,7 +5,11 @@ import './index.css';
 function ToolsMenu() {
   return (
     <div className='tools-menu-block'>
-      <motion.div className='tools-menu-carousel'>
+      <motion.div className='tools-menu-carousel'
+          drag="x" 
+          // dragConstraints={{right: 100, left: 100}} 
+          // dragTransition={{ bounceStiffness: 600, bounceDamping: 8 }} 
+      >
         <div className='tool-menu-item'>Hello</div>
         <div className='tool-menu-item'>Hello</div>
         <div className='tool-menu-item'>Hello</div>
