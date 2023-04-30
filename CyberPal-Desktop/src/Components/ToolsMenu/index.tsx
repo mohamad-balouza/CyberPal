b1e2f2ed94522bc0905082baeaae4d00db0f8b89
unknown <mohammad.balouza@gmail.com>
2023-04-30 16:32:33 +0300

    Frontend: Added the opening tool animation

--- CyberPal-Desktop/src/Components/ToolsMenu/index.tsx
+++ CyberPal-Desktop/src/Components/ToolsMenu/index.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from "framer-motion";
 import './index.css';
 
 function ToolsMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+
   return (
     <div className='tools-menu-block'>
       <motion.div className='tools-menu-carousel'
@@ -10,7 +13,16 @@ function ToolsMenu() {
           dragConstraints={{right: 0, left: -1600}} 
           dragTransition={{ bounceStiffness: 100, bounceDamping: 10 }} 
       >
-        <div className='tool-menu-item'>Hello</div>
+        <motion.div
+            layout
+            data-isOpen={isOpen}
+            initial={{ borderRadius: 50 }}
+            className="tool-menu-item"
+            onTap={() => setIsOpen(!isOpen)}
+        >
+          <motion.div layout className="child">{isOpen ? "hello" : "How are you"}</motion.div>
+          
+        </motion.div>
         <div className='tool-menu-item'>Hello</div>
         <div className='tool-menu-item'>Hello</div>
         <div className='tool-menu-item'>Hello</div>
