1a9d0de3ea594b2934f4a8344b5dbea64f265d80
unknown <mohammad.balouza@gmail.com>
2023-04-30 17:44:18 +0300

    Frontend: Tested animate presence in framer motion

--- CyberPal-Desktop/src/Components/ToolMenuItem/index.tsx
+++ CyberPal-Desktop/src/Components/ToolMenuItem/index.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 
 function ToolMenuItem() {
-    const [isOpen, setIsOpen] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
     const [x, setX] = useState<number | undefined>();
     const [y, setY] = useState<number | undefined>();
     const tool_ref = useRef<HTMLDivElement>(null);
+    const [selectedid, setSelectedid] = useState(null);
+
 
     const getPosition = () => {
         const x = tool_ref.current?.offsetLeft;
@@ -16,27 +18,44 @@ function ToolMenuItem() {
         setY(y);
     }
 
+    const handleGettingInfo = () => {
+        getPosition();
+    }
+
     useEffect(() => {
         getPosition();
       }, []);
 
     return (
-        <motion.div
-                layout
-                data-isOpen={isOpen}
-                initial={{ borderRadius: 50 }}
-                className="tool-menu-item"
-                onTap={() => setIsOpen(!isOpen)}
-            >
-            <motion.div ref={tool_ref} layout className="child">
-                <div>
-                    {isOpen ? "hello" : "How are you"}  
-                </div>
-                <h2>X: {x ?? "No result"}</h2>
-                <h2>Y: {y ?? "No result"}</h2>
+        <>
+            <motion.div
+                    layout
+                    layoutId="1"
+                    data-isOpen={isOpen}
+                    initial={{ borderRadius: 50 }}
+                    className="tool-menu-item"
+                    onTap={() => setSelectedid(1)}
+                >
+                <motion.div ref={tool_ref} layout className="child" onTap={() => handleGettingInfo()}>
+                    <div>
+                        {isOpen ? "hello" : "How are you"}  
+                    </div>
+                    <h2>X: {x ?? "No result"}</h2>
+                    <h2>Y: {y ?? "No result"}</h2>
+                </motion.div>
+
             </motion.div>
 
-        </motion.div>
+            <AnimatePresence>
+                {selectedid && (
+                <motion.div layoutId={selectedid}>
+                    <motion.h5>Hello</motion.h5>
+                    <motion.h2>team</motion.h2>
+                    <motion.button onClick={() => setSelectedid(null)} />
+                </motion.div>
+                )}
+            </AnimatePresence>
+        </>
   )
 }
 
--- CyberPal-Desktop/src/Components/ToolsMenu/index.tsx
+++ CyberPal-Desktop/src/Components/ToolsMenu/index.tsx
@@ -4,17 +4,17 @@ import './index.css';
 import ToolMenuItem from 'Components/ToolMenuItem';
 
 function ToolsMenu() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [selectedid, setSelectedid] = useState(null);
 
 
   return (
     <div className='tools-menu-block'>
       <motion.div className='tools-menu-carousel'
           drag="x" 
-          dragConstraints={{right: 0, left: -1600}} 
-          dragTransition={{ bounceStiffness: 100, bounceDamping: 10 }} 
+          // dragConstraints={{right: 0, left: -1600}} 
+          // dragTransition={{ bounceStiffness: 100, bounceDamping: 10 }} 
       >
-        <ToolMenuItem />
+        <ToolMenuItem  />
         <ToolMenuItem />
         <ToolMenuItem />
         <ToolMenuItem />
