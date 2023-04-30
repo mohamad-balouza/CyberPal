d406ec539760bcac36849cef6187d029deb5b9f6
unknown <mohammad.balouza@gmail.com>
2023-04-30 21:27:02 +0300

    Frontend: Added the signup page structure

---
+++ CyberPal-Desktop/src/Pages/SignupPage/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from 'Components/Navbar';
+import './index.css'
+        
+function SignupPage() {
+  return (
+    <div className='login-page'>
+        <Navbar />
+        <div>Signup block</div>
+    </div>
+  )
+}
+
+export default SignupPage
