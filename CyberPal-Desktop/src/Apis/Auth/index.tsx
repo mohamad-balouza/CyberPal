46c6b3306573009289af3af3cb02c73f58c2fef1
unknown <mohammad.balouza@gmail.com>
2023-05-02 02:35:52 +0300

    Frontend: Added the axios signup api call

--- CyberPal-Desktop/src/Apis/Auth/index.tsx
+++ CyberPal-Desktop/src/Apis/Auth/index.tsx
@@ -1,8 +1,18 @@
 import axios from "axios";
 
-export function login() {
+export function login(data: object) {
     return axios.post(
         "http://127.0.0.1:8000/api/v1/login/access-token",
+        data
+    ).then(
+        res => res.data
+    )
+}
+
+export function signup(data: object){
+    return axios.post(
+        "http://127.0.0.1:8000/api/v1/users",
+        data
     ).then(
         res => res.data
     )
