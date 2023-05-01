5159454574634a5f06963b4e7943c85ef49c0000
unknown <mohammad.balouza@gmail.com>
2023-05-01 21:56:20 +0300

    Frontend: Added the current page reducer

--- CyberPal-Desktop/src/Redux/store/index.tsx
+++ CyberPal-Desktop/src/Redux/store/index.tsx
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import currentPageSlice from '../slices/currentPageSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    currentPage: currentPageSlice,
+  },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
