a9634a5043d09eda347332f8a3f37f9999ab29c8
unknown <mohammad.balouza@gmail.com>
2023-05-01 22:01:45 +0300

    Frontend: Added the current page global state to the navbar

--- CyberPal-Desktop/src/Components/Navbar/index.tsx
+++ CyberPal-Desktop/src/Components/Navbar/index.tsx
@@ -2,12 +2,19 @@ import React from 'react'
 import Logo from '../../../assets/Logo.svg';
 import { Image } from 'primereact/image';
 import { Divider } from 'primereact/divider';
-import "./index.css";
+import './index.css';
 import { useNavigate } from 'react-router-dom';
+import type { RootState } from '../../Redux/store';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeCurrentPage } from '../../Redux/slices/currentPageSlice';
 
 
 function Navbar() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const current_page = useSelector((state: RootState) => state.currentPage.value); 
+  const dispatch = useDispatch();
+
+  console.log(current_page);
 
   const handleLoginNavigation = () => {
     navigate("/login")
@@ -38,7 +45,7 @@ function Navbar() {
         <div className='profile-zone'>
             <a onClick={handleSignupNavigation}>Signup</a>
             <Divider layout='vertical' style={{height: "1px"}}/>
-            <a id='chosen-one' onClick={handleLoginNavigation}>Signin</a>
+            <a className='chosen-one' onClick={handleLoginNavigation}>Signin</a>
         </div>
     </div>
   )
