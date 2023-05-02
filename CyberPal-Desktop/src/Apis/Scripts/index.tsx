4e955c1eda244a289902e910ab76cb27611ca8e2
unknown <mohammad.balouza@gmail.com>
2023-05-03 01:36:26 +0300

    Frontend: Added the get all favorite scripts API call

--- CyberPal-Desktop/src/Apis/Scripts/index.tsx
+++ CyberPal-Desktop/src/Apis/Scripts/index.tsx
@@ -32,3 +32,19 @@ export function createScript(data: string, token: string, token_type: string){
         err => console.error(err)
     );
 }
+
+export function getAllFavoriteScripts(token: string, token_type: string){
+    return axios.get(
+        `http://127.0.0.1:8000/api/v1/scripts/getfav`,
+        {
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `${token_type} ${token}`,
+            },
+        }
+    ).then(
+        res => res.data
+    ).catch(
+        err => console.error(err)
+    );
+}
