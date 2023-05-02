32f606c8fb9523903eb6f5225ccf4c7d92ce2bee
unknown <mohammad.balouza@gmail.com>
2023-05-03 01:47:16 +0300

    Frontend: Added get schedule API call

--- CyberPal-Desktop/src/Apis/Schedules/index.tsx
+++ CyberPal-Desktop/src/Apis/Schedules/index.tsx
@@ -32,3 +32,19 @@ export function createSchedule(data: string, token: string, token_type: string){
         err => console.error(err)
     );
 }
+
+export function getSchedule(schedule_id : number, token: string, token_type: string){
+    return axios.get(
+        `http://127.0.0.1:8000/api/v1/schedules/${schedule_id}`,
+        {
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `${token_type} ${token}`,
+            },
+        }
+    ).then(
+        res => res.data
+    ).catch(
+        err => console.error(err)
+    );
+}
