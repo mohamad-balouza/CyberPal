d23adcb39e7dc8fda0294ccd98dcbcf18adf6309
unknown <mohammad.balouza@gmail.com>
2023-05-03 01:35:22 +0300

    Frontend: Added the create script API call

--- CyberPal-Desktop/src/Apis/Scripts/index.tsx
+++ CyberPal-Desktop/src/Apis/Scripts/index.tsx
@@ -15,3 +15,20 @@ export function getAllScripts(token: string, token_type: string){
         err => console.error(err)
     );
 }
+
+export function createScript(data: string, token: string, token_type: string){
+    return axios.post(
+        `http://127.0.0.1:8000/api/v1/scripts`,
+        data,
+        {
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `${token_type} ${token}`,
+            },
+        }
+    ).then(
+        res => res.data
+    ).catch(
+        err => console.error(err)
+    );
+}
