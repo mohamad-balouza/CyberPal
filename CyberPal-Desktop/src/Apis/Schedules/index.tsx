0d0122d8d1cdc814ff7f57c03a6ccf909ef84003
unknown <mohammad.balouza@gmail.com>
2023-05-03 01:48:04 +0300

    Frontend: Added the update schedule API call

--- CyberPal-Desktop/src/Apis/Schedules/index.tsx
+++ CyberPal-Desktop/src/Apis/Schedules/index.tsx
@@ -48,3 +48,20 @@ export function getSchedule(schedule_id : number, token: string, token_type: str
         err => console.error(err)
     );
 }
+
+export function updateSchedule(schedule_id : number,data: string, token: string, token_type: string){
+    return axios.put(
+        `http://127.0.0.1:8000/api/v1/schedules/${schedule_id}`,
+        data,
+        {
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `${token_type} ${token}`,
+            },
+        }
+    ).then(
+        res => res.data
+    ).catch(
+        err => console.error(err)
+    );
+}
