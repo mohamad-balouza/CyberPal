102673c988e1f036945e9bc9893c99546967a740
unknown <mohammad.balouza@gmail.com>
2023-05-03 01:01:26 +0300

    Frontend: Added error classname for the input in the flag form page

--- CyberPal-Desktop/src/Pages/FlagFormPage/index.tsx
+++ CyberPal-Desktop/src/Pages/FlagFormPage/index.tsx
@@ -50,17 +50,17 @@ function FlagFormPage() {
         <form className='form-block' onSubmit={formik.handleSubmit}>
             <div className="form-input-block">
                 <div className="p-float-label" style={{width: "49.2%", display: "flex", flexDirection: "column"}}>
-                    <InputText id="flag_name" value={formik.values.flag_name} onChange={formik.handleChange}  style={{flex: "1"}}/>
+                    <InputText id="flag_name" value={formik.values.flag_name} onChange={formik.handleChange}  style={{flex: "1"}} className={formik.touched.flag_name && Boolean(formik.errors.flag_name) ? "p-invalid" : ""}/>
                     <label htmlFor="flag_name">Flag Name</label>
                     <small className="p-error">{formik.touched.flag_name && formik.errors.flag_name}</small>
                 </div>
                 <div className="p-float-label" style={{width: "49.2%", display: "flex", flexDirection:"column"}}>
-                    <InputText id="tool_id" value={formik.values.tool_id} onChange={formik.handleChange}  style={{flex: "1"}} keyfilter="int" />
+                    <InputText id="tool_id" value={formik.values.tool_id} onChange={formik.handleChange}  style={{flex: "1"}} keyfilter="int" className={formik.touched.tool_id && Boolean(formik.errors.tool_id) ? "p-invalid" : ""} />
                     <label htmlFor="tool_id">Tool ID</label>
                     <small className="p-error">{formik.touched.tool_id && formik.errors.tool_id}</small>
                 </div>
                 <div className="p-float-label" style={{width: "49.2%", display: "flex", flexDirection:"column"}}>
-                    <InputTextarea id="flag_description" value={formik.values.flag_description} onChange={formik.handleChange} rows={5} cols={30} />
+                    <InputTextarea id="flag_description" value={formik.values.flag_description} onChange={formik.handleChange} rows={5} cols={30}  className={formik.touched.flag_description && Boolean(formik.errors.flag_description) ? "p-invalid" : ""} />
                     <label htmlFor="flag_description">Flag Description</label>
                     <small className="p-error">{formik.touched.flag_description && formik.errors.flag_description}</small>
                 </div>
