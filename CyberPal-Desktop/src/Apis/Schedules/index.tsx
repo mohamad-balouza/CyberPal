623737d1fc45dc930a04bde744d20800462e6b57
unknown <mohammad.balouza@gmail.com>
2023-05-03 01:44:53 +0300

    Frontend: Added the get all schedules API call

---
+++ CyberPal-Desktop/src/Apis/Schedules/index.tsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export function getAllSchedules(token: string, token_type: string){
+    return axios.get(
+        `http://127.0.0.1:8000/api/v1/schedules`,
+        {
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `${token_type} ${token}`,
+            },
+        }
+    ).then(
+        res => res.data
+    ).catch(
+        err => console.error(err)
+    );
+}
