417fc79775f04ea8ff24bfef5bd00d7a88c60319
unknown <mohammad.balouza@gmail.com>
2023-05-02 22:24:10 +0300

    Frontend: Showed the actual username and type of the current user in the profile page

--- CyberPal-Desktop/src/Components/ProfileInfoBlock/index.tsx
+++ CyberPal-Desktop/src/Components/ProfileInfoBlock/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Avatar } from 'primereact/avatar';
 import './index.css';
+import type { RootState } from '../../Redux/store';
+import { useSelector } from 'react-redux';
 
 
 function ProfileInfoBlock() {
+  const username = useSelector((state: RootState) => state.loggedInUserInfo.username);
+  const user_type = useSelector((state: RootState) => state.loggedInUserInfo.user_type);
+
+
   return (
     <div className='profile-info-block'>
         <div className='profile-picture-block'>
             <Avatar label="P" size="xlarge" shape="circle" style={{transform: "scale(1.3)"}} />
         </div>
         <div className='profile-details-block'>
-            <h3>Username</h3>
-            <p>Account Type</p>
+            <h3>{username}</h3>
+            <p>{user_type}</p>
             <a>Edit</a>
         </div>
     </div>
