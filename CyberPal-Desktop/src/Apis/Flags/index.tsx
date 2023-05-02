f4331be5ae2ce1eeb6041a84b03ee56d987c98d3
unknown <mohammad.balouza@gmail.com>
2023-05-03 01:31:55 +0300

    Frontend: Added the get flags by tool id API call

--- CyberPal-Desktop/src/Apis/Flags/index.tsx
+++ CyberPal-Desktop/src/Apis/Flags/index.tsx
@@ -16,3 +16,18 @@ export function createFlag(data: string, token: string, token_type: string){
         err => console.error(err)
     );
 }
+
+export function getFlagsByToolId(tool_id: number){
+    return axios.get(
+        `http://127.0.0.1:8000/api/v1/flags/?tool_id=${tool_id}`,
+        {
+            headers: {
+                'content-type': 'application/json',
+            },
+        }
+    ).then(
+        res => res.data
+    ).catch(
+        err => console.error(err)
+    );
+}
