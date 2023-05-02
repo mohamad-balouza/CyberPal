24564c84577d281db6ec49a164d02163b7b7dad2
unknown <mohammad.balouza@gmail.com>
2023-05-03 01:23:52 +0300

    Frontend: Added the delete user API call

--- CyberPal-Desktop/src/Apis/Users/index.tsx
+++ CyberPal-Desktop/src/Apis/Users/index.tsx
@@ -48,3 +48,19 @@ export function updateUser(user_id:number, data: string, token: string, token_ty
         err => console.error(err)
     );
 }
+
+export function deleteUser(user_id:number, token: string, token_type: string){
+    return axios.delete(
+        `http://127.0.0.1:8000/api/v1/users/${user_id}`,
+        {
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `${token_type} ${token}`,
+            },
+        }
+    ).then(
+        res => res.data
+    ).catch(
+        err => console.error(err)
+    );
+}
