df04db9bf7e8e85c482133b222fb202431dff0db
unknown <mohammad.balouza@gmail.com>
2023-05-03 01:49:31 +0300

    Frontend: Added the delete schedule API call

--- CyberPal-Desktop/src/Apis/Schedules/index.tsx
+++ CyberPal-Desktop/src/Apis/Schedules/index.tsx
@@ -65,3 +65,19 @@ export function updateSchedule(schedule_id : number,data: string, token: string,
         err => console.error(err)
     );
 }
+
+export function deleteSchedule(schedule_id : number, token: string, token_type: string){
+    return axios.delete(
+        `http://127.0.0.1:8000/api/v1/schedules/${schedule_id}`,
+        {
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `${token_type} ${token}`,
+            },
+        }
+    ).then(
+        res => res.data
+    ).catch(
+        err => console.error(err)
+    );
+}
