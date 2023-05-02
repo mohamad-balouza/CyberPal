2a21744127e1a76e4a391c724126838f7cf7787b
unknown <mohammad.balouza@gmail.com>
2023-05-03 01:41:13 +0300

    Frontend: Added get script API call

--- CyberPal-Desktop/src/Apis/Scripts/index.tsx
+++ CyberPal-Desktop/src/Apis/Scripts/index.tsx
@@ -81,3 +81,19 @@ export function unfavoriteScript(script_favorited_id : number, token: string, to
         err => console.error(err)
     );
 }
+
+export function getScript(script_id : number, token: string, token_type: string){
+    return axios.get(
+        `http://127.0.0.1:8000/api/v1/scripts/${script_id}`,
+        {
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `${token_type} ${token}`,
+            },
+        }
+    ).then(
+        res => res.data
+    ).catch(
+        err => console.error(err)
+    );
+}
