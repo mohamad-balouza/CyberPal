487d974df3c05146a6209f2fa03ef2d59e8b0d93
unknown <mohammad.balouza@gmail.com>
2023-05-03 15:26:38 +0300

    Frontend: Added colors to the buttons in the hero section

--- CyberPal-Desktop/src/Components/HeroBlock/index.tsx
+++ CyberPal-Desktop/src/Components/HeroBlock/index.tsx
@@ -10,8 +10,8 @@ function HeroBlock() {
             <h1>CyberPal: Your Testing Buddy</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet nostrum nihil totam laboriosam odio esse, quo facilis repellendus nobis quos cupiditate sequi, sed praesentium fugit eligendi! Unde corrupti veniam eos!</p>
             <div className='hero-action-buttons'>
-                <Button label='Get Started'/>
-                <Button label='More Info' outlined/>
+                <Button label='Get Started' className='get-started-button'/>
+                <Button label='More Info' outlined className='more-info-button'/>
             </div>
         </div>
         <div className='hero-image-block'>
