39cc309e03dd88b9e0335c84993760f18e3353ef
unknown <mohammad.balouza@gmail.com>
2023-05-03 20:13:42 +0300

    Frontend: Added the nmap item menu cover

--- CyberPal-Desktop/src/Components/NmapMenuItem/index.tsx
+++ CyberPal-Desktop/src/Components/NmapMenuItem/index.tsx
@@ -1,12 +1,19 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { motion, AnimatePresence } from "framer-motion";
+import NmapLogo from "../../../assets/NmapLogo.png"
+import { Image } from 'primereact/image';
+import './index.css';
 
 
 function NmapMenuItem() {
 
     return (
-        <div>
-            "hello team"
+        <div className='tool-menu-item-block'>
+            <div className='tool-menu-item-contents'>
+                <h4>Nmap</h4>
+                <p>Scanning Ports</p>
+            </div>
+            <Image src={NmapLogo} width="100%" className='tool-menu-item-image' />
         </div>
   )
 }
