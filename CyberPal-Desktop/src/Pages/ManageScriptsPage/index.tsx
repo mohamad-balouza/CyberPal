653c24c669a4887fd244cb1184245da270c52e11
unknown <mohammad.balouza@gmail.com>
2023-05-03 04:13:57 +0300

    Frontend: Populated the scripts admin page

--- CyberPal-Desktop/src/Pages/ManageScriptsPage/index.tsx
+++ CyberPal-Desktop/src/Pages/ManageScriptsPage/index.tsx
@@ -4,10 +4,17 @@ import { Button } from 'primereact/button';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import React, { useState } from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { getAllScripts } from 'Apis/Scripts';
+import type { RootState } from '../../Redux/store';
+import { useSelector } from 'react-redux';
 import './index.css';
 
 function ManageScriptsPage() {
-  const [scripts, setScripts] = useState([]);
+  const user_token = useSelector((state: RootState) => state.userToken.access_token); 
+  const token_type = useSelector((state: RootState) => state.userToken.token_type); 
+  const queryClient = useQueryClient();
+  const scripts = useQuery(['scripts'],() => getAllScripts(user_token, token_type));
 
   return (
     <div className='admin-page-block'>
@@ -16,7 +23,7 @@ function ManageScriptsPage() {
         <AdminNavbar />
         <h3>Scripts</h3>
         <div className="card">
-            <DataTable value={scripts} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+            <DataTable value={scripts.data} loading={scripts.isLoading} emptyMessage="No Scripts Found!" paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
                     currentPageReportTemplate="{first} to {last} of {totalRecords}">
                 <Column field="id" header="ID" style={{ width: '10%' }}></Column>
                 <Column field="script_title" header="Title" style={{ width: '20%' }}></Column>
