d39aefb4a94c3f880784ed772f0aa4a71b9f660c
unknown <mohammad.balouza@gmail.com>
2023-05-03 22:38:44 +0300

    Frontend: Fixed the invoke function

--- CyberPal-Desktop/src/main/preload.ts
+++ CyberPal-Desktop/src/main/preload.ts
@@ -21,6 +21,9 @@ const electronHandler = {
     once(channel: Channels, func: (...args: unknown[]) => void) {
       ipcRenderer.once(channel, (_event, ...args) => func(...args));
     },
+    invoke: (channel: string, ...args: unknown[]): Promise<unknown> => {
+      return ipcRenderer.invoke(channel, ...args);
+    },
   },
 };
 
