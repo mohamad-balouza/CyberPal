02e16f267e0ff68bd39252615523cf0b4e23abfd
unknown <mohammad.balouza@gmail.com>
2023-05-03 03:42:41 +0300

    Frontend: Populated the admin users table

--- CyberPal-Desktop/src/Pages/ManageUsersPage/index.tsx
+++ CyberPal-Desktop/src/Pages/ManageUsersPage/index.tsx
@@ -3,11 +3,17 @@ import AdminSidebar from 'Components/AdminSidebar';
 import { Button } from 'primereact/button';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { getAllUsers } from '../../Apis/Users';
 
 function ManageUsersPage() {
   const [users, setUsers] = useState([]);
+
+  const queryClient = useQueryClient();
+  const fetched_users = useQuery(['users'], getAllUsers);
+  
 
   return (
     <div className='admin-page-block'>
@@ -16,13 +22,13 @@ function ManageUsersPage() {
         <AdminNavbar />
         <h3>Users</h3>
         <div className="card">
-            <DataTable value={users} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+            <DataTable value={fetched_users.data} loading={fetched_users.isLoading} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
                     currentPageReportTemplate="{first} to {last} of {totalRecords}">
                 <Column field="id" header="ID" style={{ width: '10%' }}></Column>
                 <Column field="username" header="Username" style={{ width: '15%' }}></Column>
                 <Column field="email" header="Email" style={{ width: '25%' }}></Column>
                 <Column field="is_active" header="Active" style={{ width: '10%' }}></Column>
-                <Column field="user_type" header="Privileges" style={{ width: '15%' }}></Column>
+                <Column field="user_type_id" header="Privileges" style={{ width: '15%' }}></Column>
             </DataTable>
         </div>
       </div>
