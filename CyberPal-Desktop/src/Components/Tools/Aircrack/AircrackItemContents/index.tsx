f2def42db87f3f9bfee8f6c0b27028ac15fd692e
unknown <mohammad.balouza@gmail.com>
2023-05-05 16:32:09 +0300

    Frontend: added the aircrack item contents component

---
+++ CyberPal-Desktop/src/Components/Tools/Aircrack/AircrackItemContents/index.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { ScrollPanel } from 'primereact/scrollpanel';
+import './index.css';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+
+
+function AircrackItemContents() {
+  const [aircrackArgs, setAircrackArgs] = useState("");
+
+
+  const handleAircrackExecution = () => {
+    
+  }
+
+  return (
+    <ScrollPanel style={{ width: '100%', height: '250px'}} className="tool-panel-block">
+        <div style={{margin: "20px", display: 'flex', flexWrap: "wrap", gap: "24px", justifyContent: "center", textAlign: "center"}}>
+            <h3 style={{width: "100%",}}>Aircrack-ng</h3>
+            <Button label="Start Searchsploit" onClick={handleAircrackExecution} />
+            <div className="p-float-label"  style={{width: "100%"}}>
+                <InputText id="searchsploit-args" value={aircrackArgs} onChange={(e) => setAircrackArgs(e.target.value)} style={{width: "100%"}} />
+                <label htmlFor="searchsploit-args">Aircrack-ng Arguments</label>
+            </div>
+        </div>
+    </ScrollPanel>
+  )
+}
+
+export default AircrackItemContents
