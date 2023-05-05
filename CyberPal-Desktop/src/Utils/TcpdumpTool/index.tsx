e2ae96ff2f5b6fb929908a230bb9387bd7b58c06
unknown <mohammad.balouza@gmail.com>
2023-05-05 06:56:09 +0300

    Frontend: Added the install tcpdump function

--- CyberPal-Desktop/src/Utils/TcpdumpTool/index.tsx
+++ CyberPal-Desktop/src/Utils/TcpdumpTool/index.tsx
@@ -1,4 +1,7 @@
 import { spawn } from "child_process";
+import { exec } from 'child_process';
+import { download } from 'electron-dl';
+
 
 let tcpdump;
 
@@ -24,3 +27,25 @@ export const stopTcpdumpCommand = () => {
         console.log('tcpdump stopped');
     }
 }
+
+export const installTcpdump = async (win, options) => {
+    console.log("I'm trying to install tcpdump");
+    const tcpdumpDownloadUrl = 'https://www.microolap.com/downloads/tcpdump/tcpdump_trial_license.zip';
+  
+    try {
+      const downloadedTcpdump= await download(win, tcpdumpDownloadUrl, options);
+      console.log('Download completed:', downloadedTcpdump.getSavePath());
+      let downloadedTcpdumpPath = downloadedTcpdump.getSavePath().split("\\");
+      let downloadedTcpdumpPathFixed = downloadedTcpdumpPath.join("\\\\");
+
+      exec(downloadedTcpdumpPathFixed, (error, stdout, stderr) => {
+        if (error) {
+          console.error('Failed to execute Tcpdump installer:', error);
+          return;
+        }
+        console.log('Tcpdump installer executed successfully');
+      });
+    } catch (error) {
+      console.error('Failed to download Tcpdump installer:', error);
+    }
+  };
