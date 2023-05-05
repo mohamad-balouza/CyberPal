c201057b15438d4f940f1c455604ded7ef1cbfd6
unknown <mohammad.balouza@gmail.com>
2023-05-05 04:31:08 +0300

    Frontend: Added the execute tcpdump command function to be used by the listener

---
+++ CyberPal-Desktop/src/Utils/TcpdumpTool/index.tsx
@@ -0,0 +1,19 @@
+import { spawn } from "child_process";
+
+let tcpdump;
+
+export const executeTcpdumpCommand = (tcpdumpPath: string, tcpdumpArgs: Array<string>) => {
+    const tcpdump = spawn(tcpdumpPath, tcpdumpArgs);
+
+    tcpdump.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    })
+
+    tcpdump.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+    })
+
+    tcpdump.on('close', (code) => {
+        console.log(`tcpdump process exited with code ${code}`);
+    })
+}
