065771329326fc136c8629fe2a9a55b1066b0337
unknown <mohammad.balouza@gmail.com>
2023-05-05 18:24:06 +0300

    Frontend: Added the execute and stop aircrack command functions to be used in the listeners

--- CyberPal-Desktop/src/Utils/AircrackTool/index.tsx
+++ CyberPal-Desktop/src/Utils/AircrackTool/index.tsx
@@ -2,6 +2,9 @@ import { app } from "electron";
 import { download } from 'electron-dl';
 import extract = require('extract-zip');
 import path = require('path');
+import { spawn } from "child_process";
+import { exec } from 'child_process';
+
 
 
 export const installAircrack = async (win, options) => {
@@ -27,4 +30,29 @@ export const installAircrack = async (win, options) => {
     } catch (error) {
       console.error('Failed to download Aircrack installer:', error);
     }
-  };
+};
+
+let aircrack;
+
+export const executeAircrackCommand = (aircrackPath: string, aircrackArgs: Array<string>) => {
+    aircrack = spawn(aircrackPath, aircrackArgs);
+
+    aircrack.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    })
+
+    aircrack.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+    })
+
+    aircrack.on('close', (code) => {
+        console.log(`aircrack process exited with code ${code}`);
+    })
+}
+
+export const stopAircrackCommand = () => {
+    if(aircrack) {
+        aircrack.kill();
+        console.log('aircrack stopped');
+    }
+}
