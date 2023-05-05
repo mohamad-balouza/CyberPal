64689a104a4fd24564cac5cf7d5e6bfa883056c1
unknown <mohammad.balouza@gmail.com>
2023-05-05 06:58:35 +0300

    Frontend: Added the install Tcpdump listener to the main process

--- CyberPal-Desktop/src/Components/Tools/Tcpdump/TcpdumpItemContents/index.tsx
+++ CyberPal-Desktop/src/Components/Tools/Tcpdump/TcpdumpItemContents/index.tsx
@@ -25,11 +25,12 @@ function TcpdumpItemContents() {
     setTcpdumpRunning(!tcpdumpRunning);
   }
 
+
   return (
     <ScrollPanel style={{ width: '100%', height: '250px'}} className="tool-panel-block">
         <div style={{margin: "20px", display: 'flex', flexWrap: "wrap", gap: "24px", justifyContent: "center", textAlign: "center"}}>
             <h3 style={{width: "100%",}}>Tcpdump</h3>
-            {/* <Button label={installingNmap ? "installing" : "install Nmap"} loading={installingNmap} onClick={handleNmapInstallation} /> */}
+            {/* <Button label="install Tcpdump" onClick={handleTcpdumpInstallation} /> */}
             <Button label={tcpdumpRunning ? "Stop Tcpdump" : "Start Tcpdump"} onClick={handleTcpdumpExecution} />
             <div className="p-float-label"  style={{width: "100%"}}>
                 <InputText id="nmap-path" value={tcpdumpPath} onChange={(e) => setTcpdumpPath(e.target.value)} style={{width: "100%"}} />
--- CyberPal-Desktop/src/main/main.ts
+++ CyberPal-Desktop/src/main/main.ts
@@ -15,7 +15,7 @@ import log from 'electron-log';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
 import { installNmap, executeNmapCommand } from '../Utils/NmapTool';
-import { executeTcpdumpCommand, stopTcpdumpCommand } from '../Utils/TcpdumpTool';
+import { executeTcpdumpCommand, installTcpdump, stopTcpdumpCommand } from '../Utils/TcpdumpTool';
 
 class AppUpdater {
   constructor() {
@@ -163,6 +163,26 @@ ipcMain.on('execute-nmap-command', (event, command) => {
   console.log("listened on execute-nmap-command channel");
   executeNmapCommand(command);
 })
+
+ipcMain.on('install-tcpdump', async () => {
+  console.log('listened on install-tcpdump channel'); 
+  const win = BrowserWindow.getFocusedWindow();
+  const savePath = app.getPath('downloads');
+  // let downloaded_file_path = '';
+  const options = {
+    savePath,
+    openFolderWhenDone: false,
+    onStarted: (dl) => {
+      console.log('Download started:', dl.getSavePath());
+    },
+    onProgress: (progress) => {
+      console.log('Download progress:', progress);
+    },
+  };
+  const result = await installTcpdump(win, options);
+  console.log(result);
+  return result;
+});
 
 ipcMain.on('start-tcpdump', (event, args) => {
   console.log("listening on start-tcpdump channel");
