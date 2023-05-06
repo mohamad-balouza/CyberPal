efbe52ae07561d60675bc05aa75e004b85a76d13
unknown <mohammad.balouza@gmail.com>
2023-05-06 18:02:21 +0300

    Frontend: Added the execute wireshark command function to be used in the listener

--- CyberPal-Desktop/src/Utils/WiresharkTool/index.tsx
+++ CyberPal-Desktop/src/Utils/WiresharkTool/index.tsx
@@ -1,5 +1,7 @@
 import { exec } from 'child_process';
 import { download } from 'electron-dl';
+import { spawn } from "child_process";
+
 
 export const installWireshark = async (win, options) => {
     console.log("I'm trying to install Wireshark");
@@ -24,3 +26,21 @@ export const installWireshark = async (win, options) => {
       console.error('Failed to download Wireshark installer:', error);
     }
 };
+
+let wireshark;
+
+export const executeWiresharkCommand = (wiresharkPath: string, wiresharkArgs: Array<string>) => {
+    wireshark = spawn(wiresharkPath, wiresharkArgs);
+
+    wireshark.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    })
+
+    wireshark.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+    })
+
+    wireshark.on('close', (code) => {
+        console.log(`wireshark process exited with code ${code}`);
+    })
+}
