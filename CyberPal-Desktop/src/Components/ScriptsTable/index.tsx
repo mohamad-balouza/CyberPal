7664f9e3d224760efe35874f67a60c04a39e1975
unknown <mohammad.balouza@gmail.com>
2023-05-08 16:10:44 +0300

    Frontend: Added get user scripts functionality to the user's scripts table

--- CyberPal-Desktop/src/Components/ScriptsTable/index.tsx
+++ CyberPal-Desktop/src/Components/ScriptsTable/index.tsx
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { useQuery } from '@tanstack/react-query';
+import { getAllScripts } from 'Apis/Scripts';
+import type { RootState } from '../../Redux/store';
+import { useSelector } from 'react-redux';
 import './index.css';
 
+
 function ScriptsTable() {
-    const[userscripts, setUserscripts] = useState([]);
+    const user_token = useSelector((state: RootState) => state.userToken.access_token); 
+    const token_type = useSelector((state: RootState) => state.userToken.token_type); 
+    const scripts = useQuery(['user_scripts'],() => getAllScripts(user_token, token_type));
 
     return (
         <div className="card profile-content-table">
             <div className="card">
-                <DataTable size='small' value={userscripts} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+                <DataTable size='small' value={scripts.data} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
                         currentPageReportTemplate="{first} to {last} of {totalRecords}" emptyMessage="No Scripts Found!">
                     <Column field="id" header="ID" style={{ width: '10%' }}></Column>
                     <Column field="script_title" header="Title" style={{ width: '30%' }}></Column>
