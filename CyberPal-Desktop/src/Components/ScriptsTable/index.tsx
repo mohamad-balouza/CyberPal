6c34d4ba716236ef57799ca1f7a844b763820d70
unknown <mohammad.balouza@gmail.com>
2023-05-08 16:38:25 +0300

    Frontend: Added buttons for action and added selection to the scripts table

--- CyberPal-Desktop/src/Components/ScriptsTable/index.tsx
+++ CyberPal-Desktop/src/Components/ScriptsTable/index.tsx
@@ -6,18 +6,26 @@ import { getAllScripts } from 'Apis/Scripts';
 import type { RootState } from '../../Redux/store';
 import { useSelector } from 'react-redux';
 import './index.css';
+import { Button } from 'primereact/button';
 
 
 function ScriptsTable() {
+    const [visible, setVisible] = useState(false);
+    const [selectedScript, setSelectedScript] = useState(null);
     const user_token = useSelector((state: RootState) => state.userToken.access_token); 
     const token_type = useSelector((state: RootState) => state.userToken.token_type); 
     const scripts = useQuery(['user_scripts'],() => getAllScripts(user_token, token_type));
+    const paginatorLeft = <Button type="button" icon="pi pi-play" text />;
+    const paginatorRight = <Button type="button" icon="pi pi-plus" text onClick={() => {console.log(selectedScript)}} />;
+
 
     return (
         <div className="card profile-content-table">
             <div className="card">
-                <DataTable size='small' value={scripts.data} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
-                        currentPageReportTemplate="{first} to {last} of {totalRecords}" emptyMessage="No Scripts Found!">
+                <DataTable size='small' value={scripts.data} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]}
+                        selectionMode="single" selection={selectedScript} onSelectionChange={(e) => setSelectedScript(e.value)}
+                        paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+                        currentPageReportTemplate="{first} to {last} of {totalRecords}" emptyMessage="No Scripts Found!"  paginatorLeft={paginatorLeft} paginatorRight={paginatorRight}>
                     <Column field="id" header="ID" style={{ width: '10%' }}></Column>
                     <Column field="script_title" header="Title" style={{ width: '30%' }}></Column>
                     <Column field="script_content" header="Content" style={{ width: '60%' }}></Column>
