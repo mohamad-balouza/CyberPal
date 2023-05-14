28c6488fcd5de1a0b69f6d387e7a97177338616c
unknown <mohammad.balouza@gmail.com>
2023-05-14 15:41:11 +0300

    Frontend: Changed the styling in the profile tables

--- CyberPal-Desktop/src/Components/Tools/Nmap/NmapItemContents/index.tsx
+++ CyberPal-Desktop/src/Components/Tools/Nmap/NmapItemContents/index.tsx
@@ -11,7 +11,9 @@ function NmapItemContents() {
   const [installingNmap, setInstallingNmap] = useState(false);
   const [nmapRunning, setNmapRunning] = useState(false);
   const [nmapPath, setNmapPath] = useState("");
-  const [nmapArgs, setNmapArgs] = useState("");
+  const [ipAddress, setIpAddress] = useState("");
+  const [port, setPort] = useState("");
+  const [nmapAdditionalArgs, setNmapAdditionalArgs] = useState("");
   const [output, setOutput] = useState("");
   const [downloadProgress, setDownloadProgress] = useState(0);
 
@@ -59,15 +61,15 @@ function NmapItemContents() {
             <h3 style={{width: "100%",}}>Nmap</h3>
             <Button label={installingNmap ? "installing" : "install Nmap"} onClick={handleNmapInstallation} />
             <Button label="Execute Nmap" onClick={handleNmapCommandExecution} />
-            <Button label="Show Output" icon="pi pi-external-link" onClick={() => setVisible(true)} />
             <div className="p-float-label"  style={{width: "100%"}}>
                 <InputText id="nmap-path" value={nmapPath} onChange={(e) => setNmapPath(e.target.value)} style={{width: "100%"}} />
                 <label htmlFor="nmap-path">Nmap Path</label>
             </div>
             <div className="p-float-label"  style={{width: "100%"}}>
-                <InputText id="nmap-args" value={nmapArgs} onChange={(e) => setNmapArgs(e.target.value)} style={{width: "100%"}} />
-                <label htmlFor="nmap-args">Nmap Arguments</label>
+                <InputText id="nmap-args" value={nmapAdditionalArgs} onChange={(e) => setNmapAdditionalArgs(e.target.value)} style={{width: "100%"}} />
+                <label htmlFor="nmap-args">Nmap Additional Arguments</label>
             </div>
+            <Button label="Show Output" icon="pi pi-external-link" onClick={() => setVisible(true)} />
         </div>
         <Dialog header="Nmap Output" visible={visible} style={{ width: '50vw' }} onHide={() => setVisible(false)}>
             {output ? output : "Run the Command to Get your output"}
